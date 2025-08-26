150cd868d2884e43f1f63004e99e959e608de9ad
DoraNotTheExplorer <95859066+jwvillareal184@users.noreply.github.com>
2025-08-26 15:36:14 +0800

    fixed the api front end

--- src/services/UserServices.jsx
+++ src/services/UserServices.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = `${process.env.REACT_APP_BACKEND_URL}user`;
+const BASE_URL = `${process.env.REACT_APP_BACKEND_URL}/user`;
 
 export const fetchUserInfo = async () => {
     const token = localStorage.getItem('token');
